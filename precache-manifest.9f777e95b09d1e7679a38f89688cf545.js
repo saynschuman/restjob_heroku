3b0c4fd0331b2c7c6445e4b3a6ed11452d0aced1
saynschuman <saynschuman@gmail.com>
2018-10-19 16:57:52 +0300

    btc added

---
+++ precache-manifest.9f777e95b09d1e7679a38f89688cf545.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c86b7ac09c2942e9845cce32b8ea7461",
+    "url": "/static/media/logo.c86b7ac0.png"
+  },
+  {
+    "revision": "aafeb23794c0546aa78e6d11e49ef908",
+    "url": "/static/media/GothaProReg.aafeb237.otf"
+  },
+  {
+    "revision": "dd3d9ca53a246950706e94726bb1403e",
+    "url": "/static/media/GothaProMed.dd3d9ca5.otf"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d2c6437fe8bb518b225c",
+    "url": "/static/js/main.d2c6437f.chunk.js"
+  },
+  {
+    "revision": "4fd91c74e51506835aa9",
+    "url": "/static/js/1.4fd91c74.chunk.js"
+  },
+  {
+    "revision": "d2c6437fe8bb518b225c",
+    "url": "/static/css/main.8025e70d.chunk.css"
+  },
+  {
+    "revision": "6c3cd43087d5b53151c21a4843c1bb93",
+    "url": "/index.html"
+  }
+];
